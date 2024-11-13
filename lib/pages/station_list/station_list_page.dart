653a0e75df4982648d60529d5c732b825c77aa08
LeeHJuu <xyident903@gmail.com>
2024-11-13 15:24:28 +0900

    first commit

---
+++ lib/pages/station_list/station_list_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class StationListPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
